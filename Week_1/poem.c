bae5fad4775d24751a74dbe4c9b6b10ced2ab07c
Djallal Mekki <djmekki@2-C-1.42heilbronn.de>
2023-06-16 00:49:04 +0200

    add files

---
+++ Week_1/poem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char *str = "Willows whiten, aspens shiver.\nThe sunbeam showers break and quiver\n\
+In the stream that runneth ever\nBy the island in the river\nFlowing down to Camelot.\n\
+Four gray walls, and four gray towers\nOverlook a space of flowers,\n\
+And the silent isle imbowers\nThe Lady of Shalott.\n";
+            
+    printf("%s", str);
+    return (0);
+}
